be6b711bf5af21443d0522e5e5648401178f7518
bn-yussuph <sulehajjaajj@gmail.com>
2025-06-26 14:50:39 +0000

    fix mongodb url issues

--- utils/db.js
+++ utils/db.js
@@ -12,12 +12,12 @@ const host = process.env.DB_HOST || 'localhost';
 const port = process.env.DB_PORT || 27017;
 const database = process.env.DB_DATABASE || 'files_manager';
 const uri = `mongodb://${host}:${port}/`;
-const uri1 = 'mongodb+srv://hajjaj:root@cluster0.h6dk4eo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+//const uri1 = 'mongodb+srv://hajjaj:root@cluster0.h6dk4eo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 class DBClient {
   constructor() {
     this.db = null;
-    client.connect(uri1, { useUnifiedTopology: true }, (err, client) => {
+    client.connect(uri, { useUnifiedTopology: true }, (err, client) => {
       if (err) console.log(`Mongodb client not connected ${err}`);
       this.db = client.db(database);
       this.db.createCollection('users');
